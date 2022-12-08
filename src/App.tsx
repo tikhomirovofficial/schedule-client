5e6dfd7439107bee47488a9fd39bca710096030d
Artem Tikhomirov <arteom.tixomirow@yandex.ru>
2022-12-08 23:33:45 +0300

    main html skelet created

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container ml-auto f-center-col h-100v">
+          <div className="w-100p shedule flex-col-betw">
+              <header className="flex-row-betw">
+                <div className="header__logo d-f al-center gap-20">
+                    <img src="img/logo.jpg" alt=""/>
+                    <p className="header__text">
+                        Череповецкий
+                        Лесомеханический
+                        техникум
+                    </p>
+                </div>
+                <div className="header__things d-f al-center gap-30">
+                    <div className="header__thing-item d-f al-center">
+                        <div className="emodji"></div>
+                        <p>Some text</p>
+                    </div>
+                    <div className="header__thing-item d-f al-center">
+                        <div className="emodji"></div>
+                        <p>Some text</p>
+                    </div>
+                    <div className="header__thing-item d-f al-center">
+                        <div className="emodji"></div>
+                        <p>Some text</p>
+                    </div>
+                    <div className="header__thing-item d-f al-center">
+                        <div className="emodji"></div>
+                        <p>Some text</p>
+                    </div>
+                </div>
+              </header>
+              <div className="shedule__content">
+                  <div className="shedule__header w-100p flex-row-betw">
+                    <div className="shedule__header-left d-f al-center">
+                        <div className="shedule__header-item shedule__header-item-choosed">
+                            1 Курс
+                        </div>
+                        <div className="shedule__header-item">
+                            1 Курс
+                        </div>
+                        <div className="shedule__header-item">
+                            1 Курс
+                        </div>
+                        <div className="shedule__header-item">
+                            1 Курс
+                        </div>
+                    </div>
+                    <div className="shedule__header-right d-f al-center">
+                        <div className="shedule__header-item shedule__header-item-choosed">
+                            а
+                        </div>
+                        <div className="shedule__header-item">
+                            а
+                        </div>
+                        <div className="shedule__header-item">
+                            а
+                        </div>
+                        <div className="shedule__header-item">
+                            а
+                        </div>
+                        <div className="shedule__header-item">
+                            а
+                        </div>
+                        <div className="shedule__header-item">
+                            а
+                        </div>
+                    </div>
+                  </div>
+              </div>
+              <div className="shedule__updated f-center-row">
+                  <p>
+                      обновлено 03.12.2022 9:57
+                  </p>
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
